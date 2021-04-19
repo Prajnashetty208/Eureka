888fd846e96d9a14991d6a41b9f0ba5298e07b2a
Prajnashetty208 <60169450+Prajnashetty208@users.noreply.github.com>
2021-04-19 13:09:31 +0530

    eureka config

--- src/main/java/com/order/eureka/repository/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.order.eureka.repository;
-
-import com.order.eureka.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepo extends JpaRepository<Order, Long> {
-}
